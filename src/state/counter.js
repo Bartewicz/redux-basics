0956b2a0ea7b0e2ab7ba2719bb117eecdbabe8b6
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-05-14 11:12:47 +0200

    Add simple passive reducer

---
+++ src/state/counter.js
@@ -0,0 +1,14 @@
+const initialState = 0
+
+export default (state = initialState, action) => {
+  return state
+}
+
+// export const reducerName = (state = , action) => {
+//   switch (action.type) {
+//     case 'ACTION_TYPE':
+//       return 
+//     default:
+//       return state
+//   }
+// }
