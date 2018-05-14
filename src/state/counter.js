769b0a4ae04cc2abd23de4f7e7abdfcfc044c934
Bartewicz <bartoszwojtalewicz@pro.wp.pl>
2018-05-14 12:41:25 +0200

    Decrease or increase counter by a number passed to reducer or 1 by default

--- src/state/counter.js
+++ src/state/counter.js
@@ -4,8 +4,8 @@ const DEC = 'counter/DEC'
 
 // Actions creators
 // This are functions that call for each Action
-export const inc = () => ({ type: INC })
-export const dec = () => ({ type: DEC })
+export const inc = (number) => ({ type: INC, number })
+export const dec = (number) => ({ type: DEC, number })
 
 // Initial state - state is empty by default
 const initialState = 0
@@ -13,9 +13,9 @@ const initialState = 0
 export default (state = initialState, action) => {
   switch (action.type) {
     case INC:
-      return state + 1
+      return state + action.number
     case DEC:
-      return state - 1
+      return state - action.number
     default:
       return state
   }
--- src/store.js
+++ src/store.js
@@ -1,14 +1,20 @@
+// Redux
 import { createStore, combineReducers } from 'redux'
-import counterReducer from './state/counter'
+// Reducers
+import counter, { inc, dec } from './state/counter'
 
 const reducer = combineReducers({
-  counter: counterReducer
-
+  // Shorter syntax for counter: counter
+  counter
 })
 
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+window.inc = (number = 1) => store.dispatch(inc(number))
+
+window.dec = (number = 1) => store.dispatch(dec(number))
 
 export default store
